09ff7823fbe11a8f0e87d82d718ce2d9c0f3d756
Matthew Adamson <matthewladamson@gmail.com>
2020-11-07 17:31:33 -0800

    Add files via upload

---
+++ models/workout.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const workoutSchema = new mongoose.Schema (
+    {
+        day: { type: Date, default: Date.now},
+        exercises: [
+            {
+                exercise_type: { type: String, trim: true },
+                name: { type: String, trim: true },
+                duration: { type: Number },
+                weight: { type: Number },
+                reps: { type: Number },
+                sets: { type: Number },
+                distance: { type: Number }
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+)
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
