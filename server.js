11a9d486d5d93a00a1e853a462c782ad1e8705c0
Matthew Adamson <matthewladamson@gmail.com>
2020-11-10 18:06:29 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 27017;
 
 const app = express();
 
